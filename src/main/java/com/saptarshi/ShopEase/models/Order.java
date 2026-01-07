ef7988d8c5045395ea530470801dc8639aa88e2d
Saptarshi <saptarshibiswas6@gmail.com>
2026-01-07 18:27:44 +0530

    initial commit

---
+++ src/main/java/com/saptarshi/ShopEase/models/Order.java
@@ -0,0 +1,25 @@
+package com.saptarshi.ShopEase.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+@Getter
+@Setter
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDateTime orderDate;
+    private String status;
+
+    // This links to the "Sales Slips"
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private List<OrderItem> items;
+}
